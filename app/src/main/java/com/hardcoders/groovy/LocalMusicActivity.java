93f63f89a46daa81ca24e6e3ae721bc7eef2775a
shiraz <mshirazab@gmail.com>
2017-04-21 21:18:34 +0530

    created first activity

---
+++ app/src/main/java/com/hardcoders/groovy/LocalMusicActivity.java
@@ -0,0 +1,14 @@
+package com.hardcoders.groovy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawableWrapper;
+
+public class LocalMusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_music);
+    }
+}
---
+++ app/src/main/java/com/hardcoders/groovy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hardcoders.groovy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPopup(View view) {
+        Intent intent = new Intent(this, LocalMusicActivity.class);
+        startActivity(intent);
+    }
+}
