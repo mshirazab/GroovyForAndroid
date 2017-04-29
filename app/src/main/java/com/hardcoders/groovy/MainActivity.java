71a051dd6f4d5beb00784d413854b20183a36e45
shiraz <mshirazab@gmail.com>
2017-04-29 12:21:57 +0530

    Seventh Commit

--- app/src/main/java/com/hardcoders/groovy/MainActivity.java
+++ app/src/main/java/com/hardcoders/groovy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hardcoders.groovy;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Debug;
 import android.provider.MediaStore;
@@ -11,18 +15,24 @@ import android.support.v7.view.menu.ExpandedMenuView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import org.cmc.music.metadata.IMusicMetadata;
+import org.cmc.music.metadata.ImageData;
 import org.cmc.music.metadata.MusicMetadata;
 import org.cmc.music.metadata.MusicMetadataSet;
 import org.cmc.music.myid3.MyID3;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MUSIC_ID = 14;
+    final public static Uri sArtworkUri = Uri.parse("content://media/external/audio/albumart");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getPath(Uri uri) {
-        String[] projection = { MediaStore.Audio.Media.DATA };
+        String[] projection = {MediaStore.Audio.Media.DATA};
         Cursor cursor = managedQuery(uri, projection, null, null, null);
         startManagingCursor(cursor);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
@@ -50,7 +60,33 @@ public class MainActivity extends AppCompatActivity {
         return cursor.getString(column_index);
     }
 
+    public long getAlbumID(Uri uri) {
+        String[] projection = {MediaStore.Audio.Media.ALBUM_ID};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        startManagingCursor(cursor);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID);
+        cursor.moveToFirst();
+        return cursor.getLong(column_index);
+    }
 
+    public static byte[] readFile(File file) throws IOException {
+        // Open file
+        RandomAccessFile f = new RandomAccessFile(file, "r");
+
+        try {
+            // Get and check length
+            long longlength = f.length();
+            int length = (int) longlength;
+            if (length != longlength) throw new IOException("File size >= 2 GB");
+
+            // Read file and return data
+            byte[] data = new byte[length];
+            f.readFully(data);
+            return data;
+        } finally {
+            f.close();
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -60,20 +96,30 @@ public class MainActivity extends AppCompatActivity {
                 File audioFile = new File(getPath(audioFileUri));
                 try {
                     MusicMetadataSet src_set = new MyID3().read(audioFile);
-                    IMusicMetadata metadata = src_set.getSimplified();
+                    MusicMetadata metadata = (MusicMetadata) src_set.getSimplified();
+                    //print artist,album,song name
                     String artist = metadata.getArtist();
                     String album = metadata.getAlbum();
                     String song_title = metadata.getSongTitle();
-                    Number track_number = metadata.getTrackNumber();
-                    Log.d("Music Selected", artist + "\t" + album + "\t" + song_title + "\t" + track_number);
+                    Log.d("Music Selected", artist + "\t" + album + "\t" + song_title + "\t");
+
+                    //show album art
+                    Uri uri = ContentUris.withAppendedId(sArtworkUri, getAlbumID(audioFileUri));
+                    Picasso.with(this).load(uri).into((ImageView) findViewById(R.id.my_image));
+
+
+                    //
+                    File img;
+                    ImageData imageData = new ImageData(readFile(img), "", "", 3);
+                    metadata.addPicture(imageData);
+                    metadata.setArtist("Bob Marley");
+
+                    //save changes
+                    //File dst = new File(audioFile.getAbsolutePath() + " (Edited)");
+                    //new MyID3().write(audioFile, dst, src_set, metadata);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-/*
-                metadata.setArtist("Bob Marley");
-
-                File dst = ...
-                new MyID3().write(src, dst, src_set, metadata);*/
             }
         }
     }
