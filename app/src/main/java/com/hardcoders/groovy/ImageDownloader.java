7c8988548a002382cc1e9475ef980fb5011ee1d7
shiraz <mshirazab@gmail.com>
2017-05-01 13:44:09 +0530

    APP Done .... XDD

--- app/src/main/java/com/hardcoders/groovy/ImageDownloader.java
+++ app/src/main/java/com/hardcoders/groovy/ImageDownloader.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -64,8 +65,8 @@ public class ImageDownloader extends AsyncTask<String, Void, byte[]> {
 
     public static String GetFileName(String artist, String title) {
         String fileName = artist + " - " + title;
-        char forbiddenCharacters[] = new char[] {'/', '\\' , ':', '?' , '*', '+', '%'};
-        for(int i = 0; i < forbiddenCharacters.length; i++) {
+        char forbiddenCharacters[] = new char[]{'/', '\\', ':', '?', '*', '+', '%'};
+        for (int i = 0; i < forbiddenCharacters.length; i++) {
             fileName = fileName.replace(forbiddenCharacters[i], ' ');
         }
         return fileName;
@@ -78,15 +79,12 @@ public class ImageDownloader extends AsyncTask<String, Void, byte[]> {
             String directoryPath = audioFile.getParent();
             String newAudioFilePath = directoryPath + "/" + newFileName;
             Log.d("Image Download", newAudioFilePath);
-            //TODO File newAudioFile = new File(newAudioFilePath);
-
-
+            File newAudioFile = new File(newAudioFilePath);
             MusicMetadataSet src_set = new MyID3().read(audioFile);
             MusicMetadata metadata = (MusicMetadata) src_set.getSimplified();
             ImageData imageData = new ImageData(bytes, "", "", 3);
             metadata.clear();
             metadata.addPicture(imageData);
-
             metadata.setSongTitle(track.Name);
             metadata.setArtist(TextUtils.join(", ", track.Artists));
             metadata.setTrackNumber(track.TrackNumber);
@@ -97,6 +95,7 @@ public class ImageDownloader extends AsyncTask<String, Void, byte[]> {
             File dst = new File(audioFile.getAbsolutePath());
             new MyID3().update(audioFile, src_set, metadata);
             Log.d("Image Download", "Done");
+            Toast.makeText(context, "Music details changed", Toast.LENGTH_SHORT).show();
 
 
             //TODO audioFile.renameTo(newAudioFile);
